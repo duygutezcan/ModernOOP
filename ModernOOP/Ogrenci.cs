57cdf59a88faad412f1c50c39f4519177d9f767f
SEM <SEM@DESKTOP-4GKM6HK>
2022-11-07 13:22:08 +0300

    Add project files.

---
+++ ModernOOP/Ogrenci.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernOOP
+{
+    internal class Ogrenci
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }    
+        public string Soyad { get; set; }
+        public string Cinsiyet { get; set; }
+        public string Cadde { get; set; }
+        public string Ilce { get; set; }
+        public string Sehir { get; set; }
+        public int KapiNo { get; set; }
+        public string Unvan()
+        {
+            if(Cinsiyet=="E")
+            {
+                return "Sn Bay " +Ad+ " " + Soyad;
+            }
+            else
+            {
+                return "Sn Bayan "+Ad + " " + Soyad;
+            }
+        }
+
+    }
+}
---
+++ ModernOOP/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci o1 = new Ogrenci();
+            o1.Ad = "Duygu";
+            o1.Soyad = "Tezcan Kantar";
+            o1.Sehir = "İstanbul";
+            o1.Cadde = "Ayvansaray";
+            o1.Id = 1;
+            o1.Ilce = "Ataşehir";
+            o1.KapiNo = 7;
+            o1.Cinsiyet = "K";
+
+
+            Ogrenci o2 = new Ogrenci();
+            o2.Ad = "Serkan";
+            o2.Soyad = "Kantar";
+            o2.Sehir = "İstanbul";
+            o2.Cadde = "Atatürk";
+            o2.Id = 1;
+            o2.Ilce = "Ataşehir";
+            o2.KapiNo = 7;
+            o2.Cinsiyet = "E";
+
+
+            Ogrenci[] ogrenciler = { o1, o2 };
+            foreach (var item in ogrenciler)
+            {
+                //Console.WriteLine(item.Ad + item.Soyad);
+                Console.WriteLine(item.Unvan());
+            }
+            Console.ReadLine();
+
+
+            /*for (int i = 0; i < ogrenciler.Length; i++)
+            {
+              Console.WriteLine(ogrenciler[i].Ad + ogrenciler[i].Soyad);
+            }
+            Console.ReadLine();*/
+
+        }
+    }
+}
